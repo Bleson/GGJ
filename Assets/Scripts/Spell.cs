5a70ea828c7c3418b94e3c7cdabc1d699355e355
Roni Juppi <roju269@gmail.com>
2016-01-29 21:53:20 +0200

    basic spell script

--- Assets/Scripts/Spell.cs
+++ Assets/Scripts/Spell.cs
@@ -3,13 +3,28 @@ using System.Collections;
 
 public class Spell : MonoBehaviour {
 
+    public string spellName = "";
+
+    public int spellId = 0;
+    public int spellDamage = 5;
+    
+    enum SpellType { Fire, Ice, Water };
+
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
 	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            //Damage logic
+        }
+    }
 }
