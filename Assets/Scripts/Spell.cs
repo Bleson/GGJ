0f541b164d4b809c5066b9cd5e6194d120d7e920
Roni Juppi <roju269@gmail.com>
2016-01-29 22:58:10 +0200

    added heal mechanics and spell cost

--- Assets/Scripts/Spell.cs
+++ Assets/Scripts/Spell.cs
@@ -4,11 +4,20 @@ using System.Collections;
 public class Spell : MonoBehaviour {
 
     public string spellName = "";
-    
+
+    public float cost = 1;
     public float spellDamage = 5;
+    public float spellHeal = 5;
     public float spellSpeed = 5;
+
+    Player caster;
     
     enum SpellType { Fire, Ice, Water };
+
+    public Spell(Player p)
+    {
+        caster = p;
+    }
 
 	// Use this for initialization
 	void Start ()
@@ -25,6 +34,7 @@ public class Spell : MonoBehaviour {
         if (other.gameObject.tag == "Player")
         {
             other.gameObject.GetComponent<Player>().TakeDamage(spellDamage);
+            caster.gameObject.GetComponent<Player>().Heal(spellHeal);
         }
     }
    
