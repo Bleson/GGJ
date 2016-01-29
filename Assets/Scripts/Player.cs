3732c330f0ffa40ff71b6902ff57b59506f0d41d
Markus Hyvärinen <double-nougat@windowslive.com>
2016-01-29 21:51:22 +0200

    asdasd

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,13 +3,65 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public int id; //Determines spell owner.
+    float maxHealth = 100;
+    float health = 100; //Current real health
+    float targetHealth; // Health after hp drain
+    float hpDrainPerSecond = 5; //How fast player gets healed or loses health.
+    bool isAlive = true;
+
+    public Spell[] spellList;
+
+    void Update()
+    {
+        if (Input.GetKeyDown("Jump"))
+        {
+            TakeDamage(5);
+            
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (isAlive)
+        {
+            if (health != targetHealth)
+            {
+                ChangeHealth(targetHealth - health);
+            }
+        }
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        targetHealth -= damageAmount;
+    }
+
+    public void Heal(int healAmount)
+    {
+        targetHealth += healAmount;
+    }
+    
+    public void CastSpell()
+    {
+
+    }
+
+    void Die()
+    {
+
+    }
+    
+    void ChangeHealth(float healthDifference)
+    {
+        if (healthDifference > hpDrainPerSecond * Time.fixedDeltaTime)
+        {
+            health += hpDrainPerSecond * Time.fixedDeltaTime;
+        }
+        else
+        {
+            health += healthDifference;
+        }
+        Debug.Log(health);
+    }
 }
