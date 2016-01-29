421164cfbe0476f3b5a68889f9c7f078184f0434
Roni Juppi <roju269@gmail.com>
2016-01-29 22:39:31 +0200

    better spells

--- Assets/Scripts/Spell.cs
+++ Assets/Scripts/Spell.cs
@@ -20,19 +20,8 @@ public class Spell : MonoBehaviour {
     {
 	}
 
-    void OnCollisionEnter2D(Collision2D other)
-    {
-        Debug.Log("hit collider");
-        if (other.gameObject.tag == "Player")
-        {
-            Debug.Log("Hit player");
-            other.gameObject.GetComponent<Player>().TakeDamage(spellDamage);
-        }
-    }
-
     void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("hit trigger");
         if (other.gameObject.tag == "Player")
         {
             Debug.Log("Hit player");
