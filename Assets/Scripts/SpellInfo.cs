2374b8395895e4deafe2cdaf60417c54911b2354
Markus Hyvärinen <double-nougat@windowslive.com>
2016-01-30 23:03:00 +0200

    Kaikkea kivaa :smile: :new_moon_with_face:

--- Assets/Scripts/SpellInfo.cs
+++ Assets/Scripts/SpellInfo.cs
@@ -10,11 +10,27 @@ public class SpellInfo : MonoBehaviour {
     public GameObject healText;
     public GameObject costText;
 
+    public Sprite patternTexture;
+    public GameObject[] patternObjects;
+
+
     void Start()
     {
         nameText.GetComponent<Text>().text = spell.spellName.ToString();
         damageText.GetComponent<Text>().text = "DMG: " + spell.spellDamage.ToString();
         healText.GetComponent<Text>().text = "HEAL: " + spell.spellHeal.ToString();
         costText.GetComponent<Text>().text = "COST: " + spell.cost.ToString();
+        for (int i = 0; i < spell.spellPattern.Length; i++)
+        {
+            if (spell.spellPattern[i])
+            {
+                if (i != 4)
+                {
+                    patternObjects[i].GetComponent<Image>().sprite = patternTexture;
+                    patternObjects[i].GetComponent<Image>().color = Color.white;
+                }
+            }
+        }
+        
     }
 }
