93d8a7a99924fc46f9f73d3699873f8f05c76a9c
Markus Hyvärinen <double-nougat@windowslive.com>
2016-01-30 21:09:29 +0200

    :ocean:

---
+++ Assets/Scripts/SpellInfo.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SpellInfo : MonoBehaviour {
+
+    public Spell spell;
+    public GameObject nameText;
+    public GameObject damageText;
+    public GameObject healText;
+    public GameObject costText;
+
+    void Start()
+    {
+        nameText.GetComponent<Text>().text = spell.spellName.ToString();
+        damageText.GetComponent<Text>().text = "DMG: " + spell.spellDamage.ToString();
+        healText.GetComponent<Text>().text = "HEAL: " + spell.spellHeal.ToString();
+        costText.GetComponent<Text>().text = "COST: " + spell.cost.ToString();
+    }
+}
