1b0ad2aaa8e3192021e464e0acc8167e24fae691
Roni Juppi <roju269@gmail.com>
2016-01-30 12:06:18 +0200

    commenting spell class

--- Assets/Scripts/Spell.cs
+++ Assets/Scripts/Spell.cs
@@ -5,14 +5,12 @@ public class Spell : MonoBehaviour {
 
     public string spellName = "";
 
-    public float cost = 1;
-    public float spellDamage = 5;
-    public float spellHeal = 5;
-    public float spellSpeed = 5;
+    public float cost = 1; //spell cost, used to damage self
+    public float spellDamage = 5; 
+    public float spellHeal = 5; // used to heal self after spell cast
+    public float spellSpeed = 5; // spell flight speed
 
-    Player caster;
-    
-    enum SpellType { Fire, Ice, Water };
+    Player caster; // used to store caster information. Casting spells requires the use of initialize method
     
 	// Use this for initialization
 	void Start ()
@@ -26,17 +24,20 @@ public class Spell : MonoBehaviour {
 
     void OnTriggerEnter2D(Collider2D other)
     {
+        // if player is hit, does damage to player and heals caster
         if (other.gameObject.tag == "Player")
         {
             other.gameObject.GetComponent<Player>().TakeDamage(spellDamage);
             caster.gameObject.GetComponent<Player>().Heal(spellHeal);
-            Destroy(gameObject);
+            Destroy(gameObject); //destroy spell
         }
     }
 
     public void Initialize(Player p)
     {
         caster = p;
+
+        // shoot spell at different directions depending on player id
         if (caster.id == 0)
         {
             gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(spellSpeed, 0);
