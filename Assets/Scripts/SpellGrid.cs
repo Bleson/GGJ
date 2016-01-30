3b0f306cbf9775f0b3f03de794216459018e1a2d
Roni Juppi <roju269@gmail.com>
2016-01-30 12:50:13 +0200

    basic spellgrid functions

--- Assets/Scripts/SpellGrid.cs
+++ Assets/Scripts/SpellGrid.cs
@@ -3,13 +3,28 @@ using System.Collections;
 
 public class SpellGrid : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    bool[] grid = new bool[9];
+
+    Player caster;
+
+
+    // Use this for initialization
+    void Start () {
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    
 	}
+
+    public void SetGrid(int hitKey, Player _caster)
+    {
+        caster = _caster;
+
+        if (grid[hitKey])
+            grid[hitKey] = false;
+        else
+            grid[hitKey] = true;
+    }
 }
