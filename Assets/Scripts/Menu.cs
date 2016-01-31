30e8def79aab23b992c31fc37e1193e767d5c1b2
Markus Hyvärinen <double-nougat@windowslive.com>
2016-01-31 13:23:34 +0200

    LOL

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+    public void StartScene(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+
+    public void ShutDown()
+    {
+        Application.Quit();
+    }
+}
